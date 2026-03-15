a822365c675a1203f36bfc7e20e5d428292b078f
millind kamble <millindkamble24@gmail.com>
2026-03-15 13:55:39 +0530

    adding laptop class into git

--- src/main/java/com/example/demo/Hellow.java
+++ src/main/java/com/example/demo/Hellow.java
@@ -1,8 +1,13 @@
 package com.example.demo;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.*;
 @Component
 public class Hellow {
+
+	@Autowired // field injection
+	private Laptopc laptopc;
 public void great() {
+	laptopc.compile();
 	System.out.println("Hello this is my first project to show the springboot work");
 }
 }
---
+++ src/main/java/com/example/demo/Laptopc.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptopc {
+	public void compile() {
+		System.out.println("im form laptop class compiling with 404 bugs");
+	}
+
+}
